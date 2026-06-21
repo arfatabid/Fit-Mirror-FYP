f66627828a0ed79e2400d516355a99c434c9713b
Arfat Abid <arfatabid409@gmail.com>
2026-06-22 00:22:21 +0500

    Restored auth_service code

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Login Function
+  Future<User?> loginUser(String email, String password) async {
+    try {
+      final userCredential = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      return userCredential.user;
+    } catch (e) {
+      throw Exception("Auth Error: $e");
     }
   }
+}
