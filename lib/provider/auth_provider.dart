e930fba2b19dceff5cf6b67d06824ef0732eec81
SyedaFatima6-code <syedafatimarehman.2003@gmail.com>
2026-06-29 12:06:02 -0700

    Signup/login functionality added

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
-import '../services/auth_service.dart'; // Sheza ki file import karegi
+import '../services/auth_service.dart';
 
 class AuthProvider extends ChangeNotifier {
   final AuthService _authService = AuthService();
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
+  // Login Function
   Future<void> login(String email, String password, BuildContext context) async {
     _isLoading = true;
-    notifyListeners(); // UI ko bataye ga ke loading ho rahi hai
-
+    notifyListeners();
     try {
       await _authService.loginUser(email, password);
-      print("Login Successful");
     } catch (e) {
-      print("Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
     }
-
     _isLoading = false;
-    notifyListeners(); // UI ko bataye ga ke loading khatam ho gayi
+    notifyListeners();
+  }
+
+  // Signup Function (Naya Code)
+  Future<void> signUp(String email, String password, BuildContext context) async {
+    // Password Validation: 8 characters limit
+    if (password.length > 8) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Password must be 8 characters or less")));
+      return;
+    }
+    
+    _isLoading = true;
+    notifyListeners();
+    try {
+      await _authService.signUpUser(email, password);
+      Navigator.pop(context); // Signup successful, go back to Login
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+    _isLoading = false;
+    notifyListeners();
   }
 }
