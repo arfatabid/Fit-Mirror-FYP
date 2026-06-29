70b14f4de1d02abbe9dc3ad665e168970f6fe20d
Arfat Abid <arfatabid409@gmail.com>
2026-06-30 02:04:51 +0500

    auth service update

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,16 +3,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Login Function
-  Future<User?> loginUser(String email, String password) async {
-    try {
-      final userCredential = await _auth.signInWithEmailAndPassword(
-          email: email,
-          password: password
-      );
-      return userCredential.user;
-    } catch (e) {
-      throw Exception("Auth Error: $e");
-    }
+  // Ye function hona chahiye
+  Future<UserCredential> signUpUser(String email, String password) async {
+    return await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  // Login wala function bhi check kar lena
+  Future<UserCredential> loginUser(String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 }
